60e22e1e6c9af8fe691e78bdcb1a3e38693b99ac
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-11 10:10:15 -0700

    Create start of business logic with static method that includes the API and hidesthe key.

--- src/main.js
+++ src/main.js
@@ -2,4 +2,10 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from "./js/exchange.js";
 
+$(document).ready(function() {
+  let newExchange = new Exchange();
+  $("#btn-calculate").click(function(){
+  })
+})
