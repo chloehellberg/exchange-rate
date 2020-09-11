d14d2e71ca26c224a84857226f1ab5afdbaffdae
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-11 14:04:49 -0700

    New approach - will try to display error from main.js and this.status will stay as is for this test.

--- src/js/exchange.js
+++ src/js/exchange.js
@@ -5,12 +5,12 @@ export default class Exchange {
       const url = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
       console.log("key", process.env.API_KEY);
       request.onload = function() {
-        if (this.status === 200 && request.response.conversion_rates[country] != undefined) {
+        if (this.status === 200) {     //&& request.response.conversion_rates[country] === undefined)
           resolve(request.response);
         } else {
           reject(request.response);
         }
-      }
+      };
       request.open("GET", url, true);
       request.send();
     });
