a1567f50baa37d145ce1860d4aeef9106ae9cb63
Chloe Hellberg <chloe.hellberg@gmail.com>
2020-09-11 11:14:58 -0700

    Save dollars and country input values into variables.

--- src/main.js
+++ src/main.js
@@ -4,8 +4,22 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Exchange from "./js/exchange.js";
 
+function clearOptions() {
+  $('#dollars').val("");
+  $('#country').val("");
+}
+
+
 $(document).ready(function() {
-  let newExchange = new Exchange();
   $("#btn-calculate").click(function(){
-  })
-})
+    let usDollars = $('#dollars').val();
+    let country = $('#country').val();
+    clearOptions();
+
+    let promise = Exchange.getExchangeRate();
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      $('#rate').text(`Rate is ${body.conversion_rates}`);
+    });
+  });
+});
